18af2b930849039d393f4e5b8b7bd25289bd1c80
xiongjunlang <xiongjunlang@agree.com.cn>
2017-07-18 17:35:09 +0800

    域名改为ip版本

--- app/src/main/java/com/hz/callanalysisengine/constant/Constant.java
+++ app/src/main/java/com/hz/callanalysisengine/constant/Constant.java
@@ -6,9 +6,9 @@ package com.hz.callanalysisengine.constant;
  */
 public class Constant {
     // Call表访问url
-    public static String CALL_URL = "http://www.liuyang-anime.cn/call/";
+    public static String CALL_URL = "http://123.207.27.244/call/";
     // 基础url
-    public static String BASE_URL = "http://www.liuyang-anime.cn/";
+    public static String BASE_URL = "http://123.207.27.244/";
     // b站call表url
     public static String BILIBILI_URL = "http://m.bilibili.com/video/av5735255.html#page=";
 }
--- app/src/main/java/com/hz/callanalysisengine/interfaces/IPostRetrofit.java
+++ app/src/main/java/com/hz/callanalysisengine/interfaces/IPostRetrofit.java
@@ -2,9 +2,13 @@ package com.hz.callanalysisengine.interfaces;
 
 import com.hz.callanalysisengine.bean.SuccessBean;
 
+import java.util.Map;
+
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Url;
@@ -14,9 +18,10 @@ import retrofit2.http.Url;
  * post请求的接口
  */
 public interface IPostRetrofit {
-    @Headers({"Content-Type: application/json",
+    @Headers({"Content-Type: application/x-www-form-urlencoded",
             "Accept: application/json"})     //需要添加头
 
+    @FormUrlEncoded
     @POST
-    Call<SuccessBean> messagePost(@Url String url, @Body RequestBody route);
+    Call<SuccessBean> messagePost(@Url String url, @FieldMap Map<String,Object> params);
 }
